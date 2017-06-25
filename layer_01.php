b0b3dd78838a4b793c052d41515bddd520adaba6
Sophia Shapira <Sophia.Shapira@gmail.com>
2017-06-24 22:47:15 -0400

    Got things up to date

---
+++ layer_01.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once(realpath(__DIR__ . "/f_jerc_create.php"));
+require_once(realpath(__DIR__ . "/o_jerc_context.php"));
+
+
--- main.php
+++ main.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once(realpath(__DIR__ . "/f_jerc_create.php"));
-require_once(realpath(__DIR__ . "/o_jerc_context.php"));
+require_once(realpath(__DIR__ . "/layer_01.php"));
 
 
